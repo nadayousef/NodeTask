be2575cbf6920d24a91aa54d27814b0103287ca7
nadayousef <48342782+nadayousef@users.noreply.github.com>
2023-09-05 00:19:00 +0300

    Add files via upload

---
+++ Authurization/Auth.js
@@ -0,0 +1,84 @@
+const User = require('../models/user')
+exports.register = async (req, res, next) => {
+  const { username, password } = req.body
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'Password less than 6 characters' })
+
+  }
+  try {
+    await User.create({
+      username,
+      password,
+    }).then(user =>
+      res.status(200).json({
+        message: "User successfully created",
+        user,
+      })
+    )
+  } catch (err) {
+    res.status(401).json({
+      message: "User not successful created",
+      error: err.mesage,
+    })
+  }
+}
+
+exports.login = async (req, res, next) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username or Password not present",
+    })
+  }
+}
+
+exports.update = async (req, res, next) => {
+  const { role, id } = req.body;
+
+  if (role && id) {
+
+    if (role === "admin") {
+
+      await User.findById(id)
+        .then((user) => {
+
+          if (user.role !== "admin") {
+            user.role = role;
+            user.save((err) => {
+
+              if (err) {
+                res
+                  .status("400")
+                  .json({ message: "An error occurred", error: err.message });
+                process.exit(1);
+              }
+              res.status("201").json({ message: "Update successful", user });
+            });
+          } else {
+            res.status(400).json({ message: "User is already an Admin" });
+          }
+        })
+        .catch((error) => {
+          res
+            .status(400)
+            .json({ message: "An error occurred", error: error.message });
+        });
+      }
+    }
+  };
+
+
+  exports.deleteUser = async (req, res, next) => {
+    const { id } = req.body
+    await User.findById(id)
+      .then(user => user.remove())
+      .then(user =>
+        res.status(201).json({ message: "User successfully deleted", user })
+      )
+      .catch(error =>
+        res
+          .status(400)
+          .json({ message: "An error occurred", error: error.message })
+      )
+  }
